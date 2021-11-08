9529b92f50aacf07e80bf4e34fbaeb09324454f3
yatsun <yatsunlam25@gmail.com>
2021-11-08 12:24:36 -0500

    commit tree 649fbab421c8a4cf92bca4f66e925211ac3595aa

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
 // create your App component here
+// http://api.open-notify.org/astros.json
+import React from 'react'
+
+export default class App extends React.PureComponent {
+
+    state={
+        people: []
+    }
+    // componentDidMount() {
+    //     fetch("http://api.open-notify.org/astros.json")
+    //       .then((response) => response.json())
+    //       .then((data) => {
+    //         this.setState({
+    //           peopleInSpace: data.people,
+    //         });
+    //       });
+    //   }
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(r => r.json())
+        .then(({people}) => this.setState({people: people}))
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.people.map((person, craft)=><h1>{person.name} {person.craft}</h1>)}
+            </div>
+        )
+    }
+}
+
